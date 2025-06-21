78f48d187f6350b05e8e37d744a0d0e23107be5f
KrukDallan <99502048+KrukDallan@users.noreply.github.com>
2025-06-21 21:11:31 +0200

    🚧

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -9,7 +9,10 @@ var can_start: bool = false
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(1)
-	
+	if is_multiplayer_authority():
+		set_physics_process(true)
+	else:
+		set_physics_process(false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
